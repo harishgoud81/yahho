e34cb2db84bc32821ea7418aa8e0ff5e3fa59354
Harish <Harish@192.168.3.74>
2019-03-26 12:47:12 +0530

    Check

---
+++ src/yahoo_testng/Mail.java
@@ -0,0 +1,25 @@
+package yahoo_testng;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Mail
+{
+public WebDriver driver;
+
+@FindBy(xpath="//*[@id='uh-mail-link']")
+public WebElement mail;
+
+public Mail(WebDriver driver)
+{
+	this.driver=driver;
+	PageFactory.initElements(driver,this);
+}
+
+public void clickmail()
+{
+	mail.click();
+}
+}
